ad2aba3e045eff6792cc2ff99c3539ad0f3ec57e
RED-offline <rrreeeddd.work@gmail.com>
2023-03-21 10:43:48 +0200

    initial commit

---
+++ lib/app/view/app.dart
@@ -0,0 +1,15 @@
+import 'package:employee_app_team/l10n/l10n.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: Placeholder(),
+    );
+  }
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:employee_app_team/app/app.dart';
+import 'package:employee_app_team/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
