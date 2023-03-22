c8bb89e75b8c9caed037ed7b778001041af64437
YannetGorob <78377338+YannetGorob@users.noreply.github.com>
2023-03-22 17:13:45 +0200

    fix: fix ui

--- lib/features/candidates/candidate_detail/presentation/candidate_detail_page.dart
+++ lib/features/candidates/candidate_detail/presentation/candidate_detail_page.dart
@@ -1,16 +1,19 @@
 import 'package:employee_app_team/features/candidates/candidate_detail/domain/candidate_detail_cubit.dart';
 import 'package:employee_app_team/features/candidates/candidate_detail/presentation/candidate_detail_widget.dart';
+import 'package:employee_app_team/features/candidates/candidates_list/data/models/candidate_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CandidateDetailPage extends StatelessWidget {
-  const CandidateDetailPage({super.key});
-
+  const CandidateDetailPage({required this.candidate, super.key});
+  final CandidateModel candidate;
   @override
   Widget build(BuildContext context) {
     return BlocProvider<CandidateDetailCubit>(
       create: (_) => CandidateDetailCubit(),
-      child: const CandidateDetailWidget(),
+      child: CandidateDetailWidget(
+        candidate: candidate,
+      ),
     );
   }
 }
